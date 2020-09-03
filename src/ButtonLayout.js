fedba12afda9d48f16d59fdc5c7b1b6085f450da
rkabilesh <70065243+rkabilesh@users.noreply.github.com>
2020-09-03 17:10:09 +0530

    Add files via upload

--- src/ButtonLayout.js
+++ src/ButtonLayout.js
@@ -3,10 +3,7 @@ export default function ButtonLayout({clickHandler}) {
 
     const operators = ["÷","+","-","x","="];
     const row1 = ["AC","+/-","%","÷"];
-    const row2 = ["7","8","9","x"];
-    const row3 = ["4","5","6","-"];
-    const row4 = ["1","2","3","+"];
-    const row5 = ["0",".","="];
+    const otherRows = ["7","8","9","x","4","5","6","-","1","2","3","+","0",".","="];
 
     function button(data) {
         return <button id={operators.includes(data) ? "orange" : (data==="0" ? "zero" : null)}
@@ -16,10 +13,7 @@ export default function ButtonLayout({clickHandler}) {
     return (
         <div className='allButtons'>
             <div className="row1">{row1.map(button)}</div>
-            <div>{row2.map(button)}</div>
-            <div>{row3.map(button)}</div>
-            <div>{row4.map(button)}</div>
-            <div>{row5.map(button)}</div>
+            <div>{otherRows.map(button)}</div>
         </div>
     )
 }
